56144906778cef73fff940e1e8fdfdcfd237269a
Professor <Professor@192.168.200.232>
2016-11-22 17:47:40 -0200

    index

--- Paginas/index.php
+++ Paginas/index.php
@@ -1,2 +1,38 @@
-<?php 
-header('Location:../Paginas/PaginaDoProfessor.php');
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <title></title>
+    </head>
+    <body>
+        <div class="jumbotron text-center">
+  <h1>Sistema de avaliação</h1>
+  <p>Faça login para acessar o sistema de avaliação.</p> 
+</div>
+<div class=" col-lg-12">
+            <div class=" col-lg-4"></div>
+            <div class=" col-lg-4">              
+                <form action="../Paginas/PaginaDoProfessor.php">
+           <h2>Login</h2>
+  <div class="form-group">
+    
+    <input type="email" class="form-control" id="email" placeholder="Email">
+  </div>
+  <div class="form-group">
+    
+    <input type="password" class="form-control" id="pwd" placeholder="Senha">
+  </div>
+  
+           <button type="submit" class="btn btn-default col-sm-3" style="float: right">Logar</button>
+</form>
+                </div>
+            <div class=" col-lg-4"></div>
+        </div>
+
+       
+    </body>
+</html>
